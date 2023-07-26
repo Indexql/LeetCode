71ba035a7bfe2ef5d84b704f6ba0a02b3d523b90
Indexql <indexql1201@gmail.com>
2023-07-27 01:17:33 +0800

    feat(linked_list): 234. Palindrome Linked List

---
+++ Data Structure/Linked List/234. Palindrome Linked List.cpp
@@ -0,0 +1,81 @@
+/*
+    Given the head of a singly linked list, return true if it is a
+    palindrome or false otherwise.
+
+
+    Example 1:
+
+
+    Input: head = [1,2,2,1]
+    Output: true
+
+    Example 2:
+
+
+    Input: head = [1,2]
+    Output: false
+
+
+    Constraints:
+
+    The number of nodes in the list is in the range [1, 105].
+    0 <= Node.val <= 9
+
+
+    Follow up: Could you do it in O(n) time and O(1) space?
+*/
+
+#include <iostream>
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode* secondHalfHead = findSecondHalfOfList(head);
+        ListNode* reversedSecondHalfHead = reverseList(secondHalfHead);
+
+        // Compare value of List head & List reversedSecondHalfHead.
+        while (reversedSecondHalfHead) {
+            if (head->val != reversedSecondHalfHead->val) {
+                return false;
+            }
+            head = head->next;
+            reversedSecondHalfHead = reversedSecondHalfHead->next;
+        }
+
+        return true;
+    }
+
+    ListNode* findSecondHalfOfList(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow->next;
+    }
+
+    ListNode* reverseList(ListNode* head) {
+        ListNode* pre = nullptr;
+        ListNode* cur = head;
+        while (cur) {
+            ListNode* tmp = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+};
+
+// TC: O(n)
+// SC: O(1)
